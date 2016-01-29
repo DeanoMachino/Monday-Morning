1a1de54cb3f57c85d60c9e903fbd1fb789957877
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-29 23:50:59 +0000

    Implemented inventory bar; started implementation of InventoryManager script.

---
+++ Monday-Morning/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Inventory/Inventory Manager")]
+
+public class InventoryManager : MonoBehaviour {
+
+	private int inventorySpaces = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
