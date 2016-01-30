02cbc23061ee9f9db46ede9acd35dd106a197482
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-30 12:26:27 +0000

    Started implementation of watermark (on-hold)

--- Monday-Morning/Assets/Scripts/Player/Inventory/InventoryManager.cs
+++ Monday-Morning/Assets/Scripts/Player/Inventory/InventoryManager.cs
@@ -6,29 +6,28 @@ using System.Collections.Generic;
 
 public class InventoryManager : MonoBehaviour {
 
-	private int inventorySpaces = 8;
-	List<InventoryItem> itemList = new List<InventoryItem>();
+	public int inventorySpaces = 8;
+	public List<InventoryItem> itemList = new List<InventoryItem>();
 	// Use this for initialization
 	void Start() {
 		itemList.Clear();
 	}
-	
-	// Update is called once per frame
-	void Update() {
-	
-	}
 
+	// Receives message
 	void ItemPickedUp(ItemType type_){
-		itemList.Add(new InventoryItem (type_));
-	}
-
-	void ItemDropped(ItemType type_){
-		foreach (InventoryItem item in itemList) {
-			if(item.itemType == type_){
-				itemList.RemoveAll(i => i.itemType == type_);
-			}
+		if (itemList.Count < inventorySpaces) {
+			itemList.Add (new InventoryItem (type_));
 		}
 	}
 
-	//void Item
+	// Receives message
+	void ItemDropped(ItemType type_){
+		if (itemList.Count > 0) {
+			foreach (InventoryItem item in itemList) {
+				if (item.itemType == type_) {
+					itemList.RemoveAll (i => i.itemType == type_);
+				}
+			}
+		}
+	}
 }
---
+++ Monday-Morning/Assets/Scripts/UI/GUIWatermark.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GUIWatermark : MonoBehaviour {
+	
+	public Texture2D watermark;
+	public Vector2 position = new Vector2(0, 0);
+	public Vector2 size = new Vector2(0, 0);
+	
+	// Use this for initialization
+	void Start () {
+		//position = new Vector2((
+
+	}
+	
+	void OnGUI(){
+		position.x = 0;
+		position.y = Screen.height - watermark.height;
+
+		//size.x = watermark.width / 4;
+		//size.y = watermark.height / 4;
+		watermark.Resize((int)size.x, (int)size.y);
+
+		GUI.DrawTexture (new Rect (position.x, position.y, watermark.width, watermark.height), watermark);
+	}
+}
