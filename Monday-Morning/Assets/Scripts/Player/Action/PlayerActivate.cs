4c8b1efb86330ca0b14e85e994a2c72beec276c2
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-30 16:29:13 +0000

    Implemented interaction tooltips with objects.

--- Monday-Morning/Assets/Scripts/Player/Action/PlayerActivate.cs
+++ Monday-Morning/Assets/Scripts/Player/Action/PlayerActivate.cs
@@ -6,17 +6,41 @@ using System.Collections;
 public class PlayerActivate : MonoBehaviour {
 	
 	public float range;
+	private string label;
+	private bool visible = false;
 
 	void Update(){
 		RaycastHit hit;
 		Ray activationRay = new Ray (transform.position, transform.forward);
-
-		if (Input.GetButtonDown ("Action")) {
-
-			// Check if object has been hit
-			if (Physics.Raycast (activationRay, out hit, range)) {
-				Debug.Log ("Object Hit");
+		visible = false;
+		// Check if object has been hit
+		if (Physics.Raycast (activationRay, out hit, range)) {
+			visible = true;
+			label = "";
+			switch(hit.collider.tag){
+				case "Cube":
+					label = "Cube";
+					// Show text tooltip
+					break;
+				default:
+					Debug.Log ("Tag hit: " + hit.collider.tag);
+					break;
 			}
+			if (Input.GetButtonDown ("Action")) {
+				Debug.Log ("Object Hit");
+				ActivateObject(hit);
+			}
+
+		}
+	}
+
+	void ActivateObject(RaycastHit hit){
+
+	}
+
+	void OnGUI(){
+		if (visible && label != "") {
+			GUI.Box(new Rect(Screen.width / 3, Screen.height * 0.6f, Screen.width / 3, Screen.height * 0.05f), label);
 		}
 	}
 }
