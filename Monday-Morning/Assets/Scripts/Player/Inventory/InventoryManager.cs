57b23fcad6833ffa64bb55822ba3d9ef2ad3f74c
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-30 00:53:28 +0000

    Adjusted InventoryItem and InventoryManager functionality.

---
+++ Monday-Morning/Assets/Scripts/Player/Inventory/InventoryManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Inventory/Inventory Manager")]
+
+public class InventoryManager : MonoBehaviour {
+
+	private int inventorySpaces = 8;
+	List<InventoryItem> itemList = new List<InventoryItem>();
+	// Use this for initialization
+	void Start() {
+		itemList.Clear();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void ItemPickedUp(ItemType type_){
+		itemList.Add(new InventoryItem (type_));
+	}
+
+	void ItemDropped(ItemType type_){
+		foreach (InventoryItem item in itemList) {
+			if(item.itemType == type_){
+				itemList.RemoveAll(i => i.itemType == type_);
+			}
+		}
+	}
+
+	//void Item
+}
