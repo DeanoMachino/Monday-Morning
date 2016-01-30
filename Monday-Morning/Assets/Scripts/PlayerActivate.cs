0e9c7becc2a4e6ab6611d7d07d8fe4a688847474
Stuart McEwan <stuart.117.1993@gmail.com>
2016-01-30 13:20:17 +0000

    Errors fixed for Raycast script, no output

--- Monday-Morning/Assets/Scripts/PlayerActivate.cs
+++ Monday-Morning/Assets/Scripts/PlayerActivate.cs
@@ -3,33 +3,42 @@ using System.Collections;
 
 public class PlayerActivate : MonoBehaviour {
 
+	public GameObject player;		// Player
+			
+	public float activateRange;		// Range of player activation
+
+	private Ray ray;				// Ray cast from player
+	private Vector3 forward;		// Forward direction for ray
+	private RaycastHit hit;			// Information for object hit by ray
+	private new Camera camera;		// Player camera
+	private Transform select;		// Object selected by ravcast
+
 	// Use this for initialization
-	void Start () {
-		RaycastHit hit;
-		Ray ray;
-		Transform camera;
-		int activateRange = 10;
+	void Start ()
+	{
+		//player = GameObject.FindGameObjectWithTag("Player");
+		camera = Camera.main;
+		ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)); // Ray from centre of screen
+		activateRange = 3;
+		forward = transform.TransformDirection (Vector3.forward);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{	
 		// Press E to activate object, this may open doors/windows, pick up an object or carry out a task
 		if(Input.GetKeyDown("e"))
 		{
-			var select = GameObject.FindWithTag("select").transform;
-			camera = Camera.current.transform;
-			ray = new Ray(camera.position, camera.forward);
+			Debug.Log("Key pressed");
+			//camera = Camera.current.transform;
+			//ray = new Ray(camera.position, camera.forward);
 
 			// If the raycast hits then activate the hit object
-			if(Physics.Raycast(ray, hit, activateRange))
+			if(Physics.Raycast(ray, out hit, activateRange))
 			{
-				hit.collider.transform.tag = "select";
 				// Activate selected object here
-				Debug.log("Object activated");
+				Debug.Log("Object activated");
 			}
 		}
-		// De-select currently selected object
-		select.tag = "none";
 	}
 }
