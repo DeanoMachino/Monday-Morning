e80fb53e298f76a1ee39cba52e8eda6deda4deb5
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-31 01:03:08 +0000

    Created GameManager; implemented item activation and tooltip code.

---
+++ Monday-Morning/Assets/Scripts/GameLogic/GameManager.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Game Logic/Game Manager")]
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance;
+	public Objectives gameObjectives;
+
+	public bool tasksCompleted = false;
+	
+
+	// Update is called once per frame
+	void Update () {
+		tasksCompleted = gameObjectives.MainObjectivesCompleted ();
+
+		if (tasksCompleted) {
+			// DO SOMETHING FOR WINNING THE GAME
+		}
+	}
+
+	void CompleteTask(GameObjectives objective_){
+		switch (objective_) {
+		case GameObjectives.WASH_TOWEL:
+			gameObjectives.washedTowel = true;
+			break;
+		case GameObjectives.FIX_SHOWER:
+			gameObjectives.fixedShower = true;
+			break;
+		case GameObjectives.HAVE_SHOWER:
+			gameObjectives.hadShower = true;
+			break;
+		case GameObjectives.WASH_CLOTHES:
+			gameObjectives.washedClothes = true;
+			break;
+		case GameObjectives.GET_DRESSED:
+			gameObjectives.gotDressed = true;
+			break;
+		case GameObjectives.DO_DISHES:
+			gameObjectives.doneDishes = true;
+			break;
+		case GameObjectives.DRY_DISHES:
+			gameObjectives.driedDishes = true;
+			break;
+		case GameObjectives.PLACE_BOWL:
+			gameObjectives.placedBowl = true;
+			break;
+		case GameObjectives.PLACE_SPOON:
+			gameObjectives.placedSpoon = true;
+			break;
+		case GameObjectives.EAT_BREAKFAST:
+			gameObjectives.ateBreakfast = true;
+			break;
+		case GameObjectives.FIND_KEYS:
+			gameObjectives.foundKeys = true;
+			break;
+		default:
+
+			break;
+		}
+	}
+
+	void Awake(){
+		Instance = this;
+	}
+}
+
+public class Objectives {
+	// MAJOR OBJECTIVES //
+	// ================ //
+	public bool hadShower = false;
+	public bool gotDressed = false;
+	public bool ateBreakfast = false;
+	public bool takenKeys = false;
+
+	// MINOR OBJECTIVES //
+	// ================ //
+	// HaveShower
+	public bool washedTowel = false;
+	public bool fixedShower = false;
+	// GetDressed
+	public bool washedClothes = false;
+	// EatBreakfast
+	public bool doneDishes = false;
+	public bool driedDishes = false;
+	public bool placedBowl = false;
+	public bool placedSpoon = false;
+	// TakeKeys
+	public bool foundKeys = false;
+	
+	public bool MainObjectivesCompleted(){
+		if (hadShower && gotDressed && ateBreakfast && takenKeys) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void ResetMainObjectives(){
+		hadShower = false;
+		gotDressed = false;
+		ateBreakfast = false;
+		takenKeys = false;
+	}
+}
+
+public enum GameObjectives {
+	NONE,
+	WASH_TOWEL,
+	FIX_SHOWER,
+	HAVE_SHOWER,		// MAJOR
+	WASH_CLOTHES,
+	GET_DRESSED,		// MAJOR
+	DO_DISHES,
+	DRY_DISHES,
+	PLACE_BOWL,
+	PLACE_SPOON,
+	EAT_BREAKFAST,		// MAJOR
+	FIND_KEYS			// MAJOR
+}
+
+public enum InteractableObjects {
+	NONE,
+	CEREAL_BOX,
+	BOWL,
+	MILK,
+	SPOON,
+	SPANNER,
+	TOWEL_DIRTY,
+	TOWEL_CLEAN,
+	BUNDLE_OF_CLOTHES,
+	KEY,
+	KITCHEN_SINK,
+	DRYING_RACK,
+	KITCHEN_TABLE,
+	WASHING_MACHINE,
+	SHOWER_UNIT,
+	DOOR,
+	CUPBOARD_DOOR,
+	DRAWER,
+	WARDROBE_DOOR
+}
+
+
--- Monday-Morning/Assets/Scripts/Player/Action/PlayerActivate.cs
+++ Monday-Morning/Assets/Scripts/Player/Action/PlayerActivate.cs
@@ -32,133 +32,249 @@ public class PlayerActivate : MonoBehaviour {
 
 		}
 	}
-	/* 
-
-Can be collected for inventory:
-- Cereal Box
-- Bowl
-- Milk
-- Spoon
-- Spanner
-- Towel
-- Bundle of clothes
-- Work outfit (e.g. suit)
-- Keys
-
-	 */
 
 	void SetTooltipLabel(RaycastHit hit){
 		switch(hit.collider.tag){
 			// INVENTORY ITEMS //
 			// =============== //
-		case "CerealBox":
-			label = "Take Box of Cereal";
-			break;
-		case "Bowl":
-			label = "Take Bowl";
-			break;
-		case "Milk":
-			label = "Take Milk";
-			break;
-		case "Spoon":
-			label = "Take Spoon";
-			break;
-		case "Spanner":
-			label = "Take Spanner";
-			break;
-		case "TowelDirty":
-			label = "Take Dirty Towel";
-			break;
-		case "TowelClean":
-			label = "Take Clean Towel";
-			break;
-		case "BundleOfClothes":
-			label = "Take Bundle of Clothes";
-			break;
-		case "Key":
-			label = "Take Key";
-			break;
+			case "CerealBox":
+				label = "Take Box of Cereal";
+				break;
+			case "Bowl":
+				label = "Take Bowl";
+				break;
+			case "Milk":
+				label = "Take Milk";
+				break;
+			case "Spoon":
+				label = "Take Spoon";
+				break;
+			case "Spanner":
+				label = "Take Spanner";
+				break;
+			case "TowelDirty":
+				label = "Take Dirty Towel";
+				break;
+			case "TowelClean":
+				label = "Take Clean Towel";
+				break;
+			case "BundleOfClothes":
+				label = "Take Bundle of Clothes";
+				break;
+			case "Key":
+				label = "Take Key";
+				break;
 
 			// INTERACTIVE ITEMS //
 			// ================= //
-		case "KitchenSink":
-			// IF DISHES NOT DONE
-				// SET LABEL TO "Wash dirty dishes"
-			break;
-		case "DryingRack":
-			// IF DISHES TO BE PUT AWAY
-				// SET LABEL TO "Put away dishes"
-			break;
-		case "KitchenTable":
-			// IF HOLDING BOWL
-				// SET LABEL TO "Place bowl"
-			// ELSE IF HOLDING SPOON
-				// SET LABEL TO "Place spoon"
-			// ELSE IF BOWL AND SPOON PLACED
-			break;
-		case "WashingMachine":
-			// IF HOLDING BUNDLE OF CLOTHES
-				// SET LABEL TO "Wash dirty clothes"
-			// ELSE IF HOLDING DIRTY TOWEL
-				// SET LABEL TO "Wash dirty towel"
-			break;
-		case "WorkOutfit":
-			// IF NOT HAD SHOWER YET
-				// SET LABEL TO "You need to take a shower first
-			// IF HAD SHOWER
-				// SET LABEL TO "Get dressed for work"
-			break;
-		case "ShowerUnit":
-			// IF SHOWER IS BROKEN AND NOT HOLDING SPANNER
-				// SET LABEL TO "The shower is broken"
-			// IF SHOWER IS BROKEN AND HOLDING SPANNER
-				// SET LABEL TO "Fix the shower"
-			// IF SHOWER IS FIXED
-				// SET LABEL TO "Take a shower
+			case "KitchenSink":
+				// IF DISHES NOT DONE
+				if(!GameManager.Instance.gameObjectives.doneDishes){
+					// SET LABEL TO "Wash dirty dishes"
+					label = "Wash dirty dishes";
+				}
+				break;
+			case "DryingRack":
+				// IF DISHES DONE AND DISHES TO BE PUT AWAY
+				if(GameManager.Instance.gameObjectives.doneDishes){
+					if(!GameManager.Instance.gameObjectives.driedDishes){
+						// SET LABEL TO "Put away dishes"
+						label = "Put away dishes";
+					}
+				}
+				break;
+			case "KitchenTable":
+				// IF HOLDING BOWL
+				if(InventoryManager.Instance.HoldingItem(ItemType.BOWL)){
+					// SET LABEL TO "Place bowl"
+					label = "Place bowl";
+				}else if(InventoryManager.Instance.HoldingItem(ItemType.SPOON)){	// ELSE IF HOLDING SPOON
+					// SET LABEL TO "Place spoon"
+					label = "Place spoon";
+				}else if((GameManager.Instance.gameObjectives.placedBowl && GameManager.Instance.gameObjectives.placedSpoon) &&
+				         !(InventoryManager.Instance.HoldingItem(ItemType.MILK) && InventoryManager.Instance.HoldingItem(ItemType.CEREAL_BOX))){	// ELSE IF ( BOWL AND SPOON PLACED ) AND NOT ( HOLDING MILK AND CEREAL BOX )
+					// SET LABEL TO "You need something to eat"
+					label = "You need something to eat";
+				}else if(GameManager.Instance.gameObjectives.placedBowl && GameManager.Instance.gameObjectives.placedSpoon &&
+				         !InventoryManager.Instance.HoldingItem(ItemType.MILK) && InventoryManager.Instance.HoldingItem(ItemType.CEREAL_BOX)){		// ELSE IF BOWL AND SPOON PLACED AND HOLDING MILK AND CEREAL BOX
+					// SET LABEL TO "Eat breakfast"
+					label = "Eat breakfast";
+				}
+				break;
+			case "WashingMachine":
+				// IF HOLDING BUNDLE OF CLOTHES
+				if(InventoryManager.Instance.HoldingItem(ItemType.BUNDLE_OF_CLOTHES)){
+					// SET LABEL TO "Wash dirty clothes"
+					label = "Wash dirty clothes";
+				}else if(InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY)){		// ELSE IF HOLDING DIRTY TOWEL
+					// SET LABEL TO "Wash dirty towel"
+					label = "Wash dirty towel";
+				}
+				break;
+			case "WorkOutfit":
+				// IF NOT HAD SHOWER YET
+				if(!GameManager.Instance.gameObjectives.hadShower){
+					// SET LABEL TO "You need to take a shower first"
+					label = "You need to take a shower first";
+				}else{	// IF HAD SHOWER
+					// SET LABEL TO "Get dressed for work"
+					label = "Get dressed for work";
+				}
+				break;
+			case "ShowerUnit":
+				// IF SHOWER IS BROKEN AND NOT HOLDING SPANNER
+				if(!GameManager.Instance.gameObjectives.fixedShower && !InventoryManager.Instance.HoldingItem(ItemType.SPANNER)){
+					// SET LABEL TO "The shower is broken"
+					label = "The shower is broken";
+				}else if(!GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.SPANNER)){	// ELSE IF SHOWER IS BROKEN AND HOLDING SPANNER
+					// SET LABEL TO "Fix the shower"
+					label = "Fix the shower";
+				}else if(GameManager.Instance.gameObjectives.fixedShower &&
+				         (!InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY) && !InventoryManager.Instance.HoldingItem(ItemType.TOWEL_CLEAN))){	// ELSE IF SHOWER IS FIXED AND NOT HOLDING TOWEL
+					// SET LABEL TO "You need a towel"
+					label = "You need a towel";
+				}else if(GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY)){	// ELSE IF SHOWER IS FIXED AND HOLDING DIRTY TOWEL
+					// SET LABEL TO "You need a CLEAN towel"
+					label = "You need a CLEAN towel";
+				}else if(GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.TOWEL_CLEAN)){	// ELSE IF SHOWER IS FIXED AND HOLDING CLEAN TOWEL
+					// SET LABEL TO "Take a shower"
+					label = "Have a shower";
+				}
+				break;
 
-			break;
-			// DEFAULT ITEMS //
-			// ============= //
-		case "Untagged":
-			label = "UNTAGGED ITEM";
-			break;
-		default:
-			Debug.Log ("Tag hit: " + hit.collider.tag);
-			break;
+				// DEFAULT ITEMS //
+				// ============= //
+			case "Untagged":
+				label = "[UNTAGGED ITEM]";
+				break;
+			default:
+				Debug.Log ("Tag hit: " + hit.collider.tag);
+				break;
 		}
 	}
 	
 
 	void ActivateObject(RaycastHit hit){
 		switch (hit.collider.tag) {
-		case "CerealBox":
-			Debug.Log ("ActivateObject -- CerealBox");
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.CEREAL_BOX, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Bowl":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.BOWL, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Milk":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.MILK, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Spoon":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.SPOON, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Spanner":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.SPANNER, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Towel":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.TOWEL, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "BundleOfClothes":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.BUNDLE_OF_CLOTHES, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "WorkOutfit":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.WORK_OUTFIT, SendMessageOptions.DontRequireReceiver);
-			break;
-		case "Key":
-			InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.KEY, SendMessageOptions.DontRequireReceiver);
-			break;
+			// INVENTORY ITEMS //
+			// =============== //
+			case "CerealBox":
+				Debug.Log ("ActivateObject -- CerealBox");
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.CEREAL_BOX, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "Bowl":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.BOWL, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "Milk":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.MILK, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "Spoon":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.SPOON, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "Spanner":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.SPANNER, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "TowelDirty":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.TOWEL_DIRTY, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "TowelClean":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.TOWEL_CLEAN, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "BundleOfClothes":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.BUNDLE_OF_CLOTHES, SendMessageOptions.DontRequireReceiver);
+				break;
+			case "Key":
+				InventoryManager.Instance.SendMessage("ItemPickedUp", ItemType.KEY, SendMessageOptions.DontRequireReceiver);
+				break;
+			
+			// INTERACTIVE ITEMS //
+			// ================= //
+			case "KitchenSink":
+				// IF DISHES NOT DONE
+				if(!GameManager.Instance.gameObjectives.doneDishes){
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.DO_DISHES, SendMessageOptions.DontRequireReceiver);
+					// do nothing
+				}
+				break;
+			case "DryingRack":
+				// IF DISHES DONE AND DISHES TO BE PUT AWAY
+				if(GameManager.Instance.gameObjectives.doneDishes){
+					if(!GameManager.Instance.gameObjectives.driedDishes){
+						// Dry dishes
+						GameManager.Instance.SendMessage("CompleteTask", GameObjectives.DRY_DISHES, SendMessageOptions.DontRequireReceiver);
+					}
+				}
+				break;
+			case "KitchenTable":
+				// IF HOLDING BOWL
+				if(InventoryManager.Instance.HoldingItem(ItemType.BOWL)){
+					// Place bowl on table and remove from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.PLACE_BOWL, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.BOWL, SendMessageOptions.DontRequireReceiver);
+				}else if(InventoryManager.Instance.HoldingItem(ItemType.SPOON)){	// ELSE IF HOLDING SPOON
+					// Place spoon on table and remove from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.PLACE_SPOON, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.SPOON, SendMessageOptions.DontRequireReceiver);
+				}else if((GameManager.Instance.gameObjectives.placedBowl && GameManager.Instance.gameObjectives.placedSpoon) &&
+				         !(InventoryManager.Instance.HoldingItem(ItemType.MILK) && InventoryManager.Instance.HoldingItem(ItemType.CEREAL_BOX))){	// ELSE IF ( BOWL AND SPOON PLACED ) AND NOT ( HOLDING MILK AND CEREAL BOX )
+					// do nothing
+				}else if(GameManager.Instance.gameObjectives.placedBowl && GameManager.Instance.gameObjectives.placedSpoon &&
+				         !InventoryManager.Instance.HoldingItem(ItemType.MILK) && InventoryManager.Instance.HoldingItem(ItemType.CEREAL_BOX)){		// ELSE IF BOWL AND SPOON PLACED AND HOLDING MILK AND CEREAL BOX
+					// Eat breakfast and remove cereal and milk from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.EAT_BREAKFAST, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.CEREAL_BOX, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.MILK, SendMessageOptions.DontRequireReceiver);
+				}
+				break;
+			case "WashingMachine":
+				// IF HOLDING BUNDLE OF CLOTHES
+				if(InventoryManager.Instance.HoldingItem(ItemType.BUNDLE_OF_CLOTHES)){
+					// Wash dirty clothes and remove from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.WASH_CLOTHES, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.BUNDLE_OF_CLOTHES, SendMessageOptions.DontRequireReceiver);
+				}else if(InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY)){		// ELSE IF HOLDING DIRTY TOWEL
+					// Wash dirty towel and remove from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.WASH_TOWEL, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.TOWEL_DIRTY, SendMessageOptions.DontRequireReceiver);
+				}
+				break;
+			case "WorkOutfit":
+				// IF NOT HAD SHOWER YET
+				if(!GameManager.Instance.gameObjectives.hadShower){
+					// do nothing
+				}else{	// IF HAD A SHOWER
+					// Get dressed for work
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.GET_DRESSED, SendMessageOptions.DontRequireReceiver);;
+				}
+				break;
+			case "ShowerUnit":
+				// IF SHOWER IS BROKEN AND NOT HOLDING SPANNER
+				if(!GameManager.Instance.gameObjectives.fixedShower && !InventoryManager.Instance.HoldingItem(ItemType.SPANNER)){
+					// do nothing
+				}else if(!GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.SPANNER)){	// ELSE IF SHOWER IS BROKEN AND HOLDING SPANNER
+					// Fix the shower and remove spanner from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.FIX_SHOWER, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.SPANNER, SendMessageOptions.DontRequireReceiver);
+				}else if(GameManager.Instance.gameObjectives.fixedShower &&
+				        (!InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY) && !InventoryManager.Instance.HoldingItem(ItemType.TOWEL_CLEAN))){	// ELSE IF SHOWER IS FIXED AND NOT HOLDING TOWEL
+					// do nothing
+				}else if(GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.TOWEL_DIRTY)){	// ELSE IF SHOWER IS FIXED AND HOLDING DIRTY TOWEL
+					// do nothing
+				}else if(GameManager.Instance.gameObjectives.fixedShower && InventoryManager.Instance.HoldingItem(ItemType.TOWEL_CLEAN)){	// ELSE IF SHOWER IS FIXED AND HOLDING CLEAN TOWEL
+					// Have shower and remove clean towel from inventory
+					GameManager.Instance.SendMessage("CompleteTask", GameObjectives.HAVE_SHOWER, SendMessageOptions.DontRequireReceiver);
+					InventoryManager.Instance.SendMessage("ItemDropped", ItemType.TOWEL_CLEAN, SendMessageOptions.DontRequireReceiver);
+				}
+				break;
+
+			// DEFAULT ITEMS //
+			// ============= //
+			case "Untagged":
+				label = "[UNTAGGED ITEM]";
+				break;
+			default:
+				Debug.Log ("Tag hit: " + hit.collider.tag);
+				break;
 		}
 	}
 
