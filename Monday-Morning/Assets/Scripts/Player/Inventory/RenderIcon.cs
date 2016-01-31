7aa6a48ca44eeb2eca0728aad8025609310d8c51
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-31 11:20:20 +0000

    Fixed inventory UI.

---
+++ Monday-Morning/Assets/Scripts/Player/Inventory/RenderIcon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Player/Inventory/Render Icon")]
+
+public class RenderIcon : MonoBehaviour {
+	//private Image image;
+
+	void Start(){
+
+	}
+
+	public void ShowIcon(){
+
+	}
+
+	public void HideIcon(){
+		//GetComponent<Renderer>().enabled = false;
+	}
+}
