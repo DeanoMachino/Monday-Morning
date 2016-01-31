225aabdab62273cfceae86fd901a13eb02a6a147
Stuart McEwan <stuart.117.1993@gmail.com>
2016-01-31 13:08:19 +0000

    Doors open on button press, key frame animations aren't implemented as they move position while animating(couldn't fix in time, animated in engine instead)

--- Monday-Morning/Assets/Scripts/Environment/DoorActivate.cs
+++ Monday-Morning/Assets/Scripts/Environment/DoorActivate.cs
@@ -6,17 +6,24 @@ using System.Collections.Generic;
 
 public class DoorActivate : MonoBehaviour {
 
-	private bool doorOpen;			// Holds if door is open or closed
-	private bool doorRotating;		// Holds if door is currently opening or closing, or not
-	private float rotationCounter;	// Counts how far door has opened so far
-	private GameObject pivotPoint;	// Pivot point for rotating door around (Hinges)
+	private bool doorOpen;										// Holds if door is open or closed
+	private bool doorRotating;									// Holds if door is currently opening or closing, or not
+	private float rotationCounter;								// Counts how far door has opened so far
+	private GameObject pivotPoint;								// Pivot point for rotating door around (Hinges)
 	
-	private AudioSource audio;		// Holds AudioSource
-	private bool audioPlayed;		// Checks if audio has been played so it only plays once (For narration)
+	private AudioSource audio;									// Holds AudioSource
+	private bool audioPlayed;									// Checks if audio has been played so it only plays once (For narration)
 
-	public float doorMaxRotation;	// Sets how far door can open in degrees
-	public float rotationVelocity;	// Velocity value for opening/closing door
+	public float doorMaxRotation;								// Sets how far door can open in degrees
+	public float rotationVelocity;								// Velocity value for opening/closing door
 
+	private Animator anim;										// Holds the animator component
+
+	private int openedHash, closedHash, openHash, closeHash;	// Hash maps for animation clips
+
+	Vector3 localPos;											// Fix for animated objects assets moving to parent position
+	bool wasPlaying;
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -29,10 +36,20 @@ public class DoorActivate : MonoBehaviour {
 		pivotPoint = new GameObject("pivot");
 		pivotPoint.transform.position = this.transform.position;
 		pivotPoint.transform.parent = this.transform;
-		pivotPoint.transform.localPosition = new Vector3(-0.5f, 0.0f, 0.0f);
+		pivotPoint.transform.localPosition = new Vector3(0.1f, 0.0f, 0.0f);
 
 		audio = GetComponent<AudioSource>();
 		audioPlayed = false;
+
+		//anim = GetComponent<Animator>();
+
+		//openedHash = Animator.StringToHash ("Opened");
+		//closedHash = Animator.StringToHash ("Closed");
+		//openHash = Animator.StringToHash ("Open");
+		//closeHash = Animator.StringToHash ("Close");
+
+		//localPos = this.transform.position;
+		//anim.Play(closedHash, -1, 0.0f);
 	}
 
 	void Rotate()
@@ -59,33 +76,57 @@ public class DoorActivate : MonoBehaviour {
 			rotationCounter = 0;	// Reset counter
 		}
 	}
-
-	// Called on object activation
-	void Activate()
+	/*
+	void Animate()
 	{
-		// Sets door to rotate when activated
-		Debug.Log ("Door activated");
-		doorRotating = true;
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		// Remove this key press when activate is finished - Stuart
-		if(Input.GetKeyDown("z") && (doorRotating == false))
+		if (doorOpen == false)
 		{
+			// Play animation forwards to open
+			anim.Play(openHash, -1, 0.0f);
+		}
+		else if (doorOpen == true)
+		{
+			// Play animation bacwards to close
+			anim.Play(closeHash, -1, 0.0f);
+		}
+
+		if ((anim.GetCurrentAnimatorClipInfo(0).GetHashCode() == openedHash) || (anim.GetCurrentAnimatorClipInfo(0).GetHashCode() == closedHash))
+		{
+			doorRotating = false;
+		}
+	}
+	*/
+	// Called on object activation
+	public void Activate()
+	{
+		// Sets door to rotate when activated if it's not already rotating
+		if(doorRotating == false)
+		{
+			Debug.Log ("Door activated");
 			doorRotating = true;
+
 			if(!audioPlayed)
 			{
 				audio.Play();
 				audioPlayed = true;
 			}
 		}
+	}
 
+	// Update is called once per frame
+	void Update()
+	{
 		// Rotate door if doorRotating == true
 		if(doorRotating == true)
 		{
 			Rotate();
+			//Animate();
 		}
 	}
+
+	/*void LateUpdate()
+	{
+		this.transform.position = localPos;
+		//transform.localPosition += localPos;
+	}*/
 }
